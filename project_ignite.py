13812ef5a65c3386f0f341e94f2402d80efccaa5
T. Montoril de Campos <114262710+montorilcampos@users.noreply.github.com>
2024-04-22 15:09:55 -0400

    Update project_ignite.py

--- project_ignite.py
+++ project_ignite.py
@@ -9,6 +9,10 @@ from nltk.tokenize import word_tokenize
 import streamlit as st
 import string
 from textblob import TextBlob
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('popular')
+nltk.download('stopwords')
 
 
 # Load the JSON data
@@ -37,19 +41,8 @@ reviews_df = pd.DataFrame(flat_data)
 reviews_df['cleaned_comments'] = reviews_df['Comment'].apply(clean_text)
 
 
-# Print the names of the columns
-print(reviews_df.columns)
 
-# Display the first few rows of the DataFrame to verify its structure
-reviews_df.head()
-
-
-
-nltk.download('popular')
-nltk.download('stopwords')
-
-
-# Handle Missing Values
+# Handling Missing Values
 
 # For numerical columns, convert to float and replace NaN with the column mean
 numerical_cols = ['Quality', 'Difficulty']
@@ -62,33 +55,30 @@ categorical_cols = ['For Credit', 'Would Take Again', 'Textbook']
 for col in categorical_cols:
     reviews_df[col].fillna('Unknown', inplace=True)
 
-# Encode Categorical Data
+
+
+# Encoding Categorical Data
+
 # One-hot encoding for the 'department' column
 reviews_df = pd.get_dummies(reviews_df, columns=['department'])
 
-# Display the first few rows to verify changes
-reviews_df.head()
-
-
-from textblob import TextBlob
 
 # Sentiment Analysis with TextBlob
 reviews_df['sentiment'] = reviews_df['Comment'].apply(lambda comment: TextBlob(comment).sentiment.polarity)
 
-# Aggregate Features
-# Define the columns for which we want to calculate aggregate statistics
+
+# Aggregating Features
+
+# Define the columns to calculate aggregate statistics
 columns_to_aggregate = ['Quality', 'Difficulty', 'sentiment']
 
 # Group by professor and aggregate with mean, median, and std
 aggregated_features = reviews_df.groupby('professor')[columns_to_aggregate].agg(['mean', 'median', 'std']).reset_index()
 
-# Flattening the multiindex for easier column access
+# Flattening the multiindex
 aggregated_features.columns = ['_'.join(col).strip() for col in aggregated_features.columns.values]
 
-# Display the aggregated features to verify
-print(aggregated_features.head())
 
-print(aggregated_features.columns)
 
 # Categorizing 'sentiment'
 
@@ -100,7 +90,6 @@ def sentiment_category(score):
     else:
         return 'Neutral'
 
-# Categorize sentiment
 reviews_df['sentiment_category'] = reviews_df['sentiment'].apply(sentiment_category)
 
 # Calculate the percentage of each sentiment category per professor
@@ -108,9 +97,6 @@ sentiment_distribution = reviews_df.groupby('professor')['sentiment_category'].v
 
 # Multiply by 100 for percentage representation and reset index for readability
 sentiment_distribution = (sentiment_distribution * 100).reset_index()
-
-# Display the sentiment distribution for each professor
-print(sentiment_distribution)
 
 
 
@@ -148,7 +134,7 @@ reviews_df['Difficulty'].fillna(reviews_df['Difficulty'].mean(), inplace=True)
 reviews_df['Would Take Again'].fillna(0, inplace=True)
 reviews_df['sentiment'].fillna(0, inplace=True) # Neutral sentiment if missing
 
-# Aggregate features by professor, also considering course if we need to filter by it later
+# Aggregate features by professor, also considering course
 aggregated_features = reviews_df.groupby(['professor', 'course_id']).agg({
     'Quality': 'mean',
     'Difficulty': 'mean',
@@ -162,7 +148,6 @@ def get_best_professor(department=None, course_id=None, df=aggregated_features):
         df = df[df['course_id'] == course_id]
 
     if df.empty:
-        # If no matches found, return a message indicating this
         return f"No data found for the given criteria."
 
     # Calculate the composite score
@@ -175,14 +160,7 @@ def get_best_professor(department=None, course_id=None, df=aggregated_features):
 
     return best_professor[['professor']]
 
-# Example
-best_prof_for_course = get_best_professor(course_id=' FHS01')
 
-print("Best Professor for Course:", best_prof_for_course)
-
-
-# Import TF-IDF
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Fix professor names
 reviews_df['professor'] = reviews_df['professor'].str.strip().str.lower()
@@ -192,9 +170,6 @@ def preprocess_text(text):
     # Replace punctuation with space
     text = ''.join([char if char.isalnum() or char.isspace() else ' ' for char in text])
     return text
-
-# Preprocess comment column
-#reviews_df['cleaned_comments'] = reviews_df['Comment'].apply(preprocess_text)
 
 # Misspelled words and words to avoid
 extended_stop_words = list(set(stopwords.words('english')) | {'ive', 'youll', 'alot', 'isnt', 'doesnt', 'wasnt',
@@ -264,7 +239,7 @@ def get_top_keywords(professor, reviews_df, top_n=5, boost_factor=1.5):
 
 
 
-# Now, let's integrate Streamlit for the user interface
+# Streamlit
 st.title("Find the Best Professor for Your Course")
 
 # Flatten the data to get a list of unique course IDs for the dropdown
@@ -280,16 +255,14 @@ selected_course_id = st.selectbox("Select a Course", course_ids)
 # When a course is selected, display the best professor for that course
 if st.button("Find Best Professor"):
     best_professor = get_best_professor(course_id=selected_course_id)
-    professor_name = best_professor.iloc[0]  # Added .strip() to remove any leading/trailing spaces
+    professor_name = best_professor.iloc[0]
     prof_keywords = get_top_keywords(professor_name, reviews_df, boost_factor=2)
-    # If prof_keywords is a list of lists and you need it to be a single list of strings
+    # Making prof_keywords a single list of strings
     prof_keywords = [keyword for sublist in prof_keywords for keyword in sublist]
 
     st.write(f"The best professor for {selected_course_id} is: {professor_name}")
+
+    # Print keywords
     if prof_keywords:
         st.markdown("Keywords for " + professor_name + " are:")
         st.markdown("* " + "\n* ".join(prof_keywords))
-# Example
-#best_prof_for_course = get_best_professor(course_id=' FHS01')
-
-#print("Best Professor for Course:", best_prof_for_course)
